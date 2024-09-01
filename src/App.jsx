e98417b46c832c6197b4d6671f8d3dc4f61ace14
RomanLatipov <romanlatipov@gmail.com>
2024-08-31 23:41:43 -0400

    create wordle grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+import words from './wordlist.js';
 
 function App() {
-  return (<></>)
+  const [word, setWord] = useState(words[Math.floor(Math.random() * words.length)]);
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
+
+  return (<>
+    {
+      guesses.map(guess => {
+        return (
+          <Line guess={guess ?? ''}/>
+        )
+      })
+    }
+  </>)
+}
+function Line({guess}) {
+  const tiles = [];
+  for (let i=0; i<5; i++) {
+    const char = guess[i];
+    tiles.push(<div key={i} className='tiles'>{char}</div>)
+  }
+
+  return <div className='line'>{tiles}</div>
 }
 
 export default App
+
