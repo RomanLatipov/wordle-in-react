f4736d1cd04852d396152365dde1c3ff39263822
RomanLatipov <romanlatipov@gmail.com>
2024-09-03 23:40:11 -0400

    end game and display correct colors when guess is correct

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,28 @@ function App() {
   const [word, setWord] = useState(words[Math.floor(Math.random() * words.length)]);
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState('');
+  const [gameOver, setGameOver] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
   useEffect(() => {
     function handleTpye(event) {
+      if (gameOver) {
+        return;
+      }
       if (event.key === 'Enter') {
         if(currentGuess.length !== 5) {
           return;
         }
-        else {
-          const idx = guesses.indexOf(null);
-          // console.log(idx); when idx == -1 end the game
-          guesses[idx] = currentGuess;
-          setCurrentGuess('');
+        else if (word === currentGuess) {
+          setGameOver(true);
+          setIsCorrect(true)
+          window.removeEventListener('keydown', handleTpye);
+          console.log("Game over");
         }
+        const idx = guesses.indexOf(null);
+        // console.log(idx); when idx == -1 end the game
+        guesses[idx] = currentGuess;
+        setCurrentGuess('');
       }
       else if (event.key === 'Backspace') {
         setCurrentGuess(oldGuess => oldGuess.substring(0, oldGuess.length-1));
@@ -34,12 +43,13 @@ function App() {
   }, [currentGuess])
 
   return (<>
+    {word}
     <div className='board'>
       {
         guesses.map((guess, i) => {
           const idx = guesses.indexOf(null);
           return (
-            <Line guess={i == idx ? currentGuess : guess ?? ''}/>
+            <Line guess={i == idx ? currentGuess : guess ?? ''} word={word} isGameOver={gameOver}/>
           )
         })
       }
@@ -47,15 +57,14 @@ function App() {
     {currentGuess}
   </>)
 }
-function Line({guess}) {
+function Line({guess, word, isGameOver}) {
   const tiles = [];
   for (let i=0; i<5; i++) {
     const char = guess[i];
-    tiles.push(<div key={i} className='tiles'>{char}</div>)
+    tiles.push(<div key={i} className='tiles' style={isGameOver && word == guess ? {background: 'green'} : {background: 'white'}}>{char}</div>)
   }
-
   return <div className='line'>{tiles}</div>
 }
-
+//
 export default App
 
