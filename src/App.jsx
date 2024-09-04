7d446e10d3907a3d4a0240b33e058105da85d7d8
RomanLatipov <romanlatipov@gmail.com>
2024-09-03 22:55:18 -0400

    guesses now appear in their respective tiles

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,42 @@ function App() {
 
   useEffect(() => {
     function handleTpye(event) {
-      setCurrentGuess(currentGuess => currentGuess + event.key);
+      if (event.key === 'Enter') {
+        if(currentGuess.length !== 5) {
+          return;
+        }
+        else {
+          const idx = guesses.indexOf(null);
+          // console.log(idx); when idx == -1 end the game
+          guesses[idx] = currentGuess;
+          setCurrentGuess('');
+        }
+      }
+      else if (event.key === 'Backspace') {
+        setCurrentGuess(oldGuess => oldGuess.substring(0, oldGuess.length-1));
+      }
+      else if (currentGuess.length < 5) {
+        setCurrentGuess(oldGuess => oldGuess + event.key);
+      }
+      // setCurrentGuess(currentGuess => currentGuess + event.key);
     }
     window.addEventListener('keydown', handleTpye);
 
-    // return () => window.removeEventListener('keydown', handleTpye)
-  }, [])
+    return () => window.removeEventListener('keydown', handleTpye);
+  }, [currentGuess])
 
   return (<>
     <div className='board'>
       {
-        guesses.map(guess => {
+        guesses.map((guess, i) => {
+          const idx = guesses.indexOf(null);
           return (
-            <Line guess={guess ?? ''}/>
+            <Line guess={i == idx ? currentGuess : guess ?? ''}/>
           )
         })
       }
-      {currentGuess}
     </div>
+    {currentGuess}
   </>)
 }
 function Line({guess}) {
